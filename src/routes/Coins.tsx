dd7349d794c310ad2cc6dafbcd1adca6ecf46acf
HJRYU\o__hy <o__hyeah@naver.com>
2023-02-27 10:24:09 +0900

    style: Coins styling with dummy data

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,9 +1,81 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
 
 export default function Coins() {
   return (
-    <>
-      <h1>Coins</h1>
-    </>
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      <CoinsList>
+        {coins.map((coin) => (
+          <Link key={coin.id} to={`/${coin.id}`}>
+            <Coin>{coin.name} &rarr;</Coin>
+          </Link>
+        ))}
+      </CoinsList>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  margin-bottom: 10px;
+  padding: 20px;
+  border-radius: 15px;
+  a {
+    transition: color 1s ease-in;
+  }
+  &:hover {
+    color: ${(props) => props.theme.accentColor};
+  }
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
